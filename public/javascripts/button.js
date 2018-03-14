ba78f1d5d3361e25f13bc9743d130560f26e593b
ia15076 <ia15076@s.inf.shizuoka.ac.jp>
2018-03-14 13:02:20 +0900

    first commit

---
+++ public/javascripts/button.js
@@ -0,0 +1,154 @@
+const socket = io.connect('/');
+
+const decorations = [
+    {
+        min: 10,
+        max: Number.MAX_SAFE_INTEGER,
+        $obj: $('.light')
+    },
+    {
+        min: 50,
+        max: Number.MAX_SAFE_INTEGER,
+        $obj: $('.audience1')
+    },
+    {
+        min: 200,
+        max: Number.MAX_SAFE_INTEGER,
+        $obj: $('.audience2')
+    },
+    {
+        min: 500,
+        max: Number.MAX_SAFE_INTEGER,
+        $obj: $('#bg')
+    },
+    {
+        min: 100,
+        max: 1000,
+        $obj: $('.silver')
+    },
+    {
+        min: 1000,
+        max: 10000,
+        $obj: $('.purple')
+    },
+    {
+        min: 10000,
+        max: Number.MAX_SAFE_INTEGER,
+        $obj: $('.gold')
+    }
+];
+
+$(document).on('keydown', function (e) {
+    if (e.keyCode == 72) {
+        $('#he-button').click();
+    }
+});
+
+$('#he-button').on('click', function () {
+    var count = parseInt($('#he-counter').text(), 10) + 1;
+    socket.emit('increment');
+
+    animateButton();
+    playSound(count);
+    kiriban(count);
+    updateCounter(count);
+    updateBackground(count);
+
+    window.navigator.vibrate(100);
+});
+
+socket.on('join', function (num) {
+    updateOthers(num);
+});
+
+socket.on('leave', function (num) {
+    updateOthers(num);
+});
+
+socket.on('increment', function (count) {
+    pop();
+    playSound(count);
+    updateCounter(count);
+    updateBackground(count);
+});
+
+function playSound(count) {
+    var audio = $('.he-sound')[count % 2];
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function kiriban(count) {
+    if (isKiriban(count)) {
+        $kiriban = $('#kiriban');
+        $kiriban.css({ 'visibility': 'visible' });
+        setTimeout(function () {
+            $kiriban.css({ 'visibility': 'hidden' });
+        }, 2000);
+    }
+}
+
+function updateCounter(count) {
+    $('#he-counter').text(count);
+}
+
+function updateBackground(count) {
+    var duration = 1000 / count;
+    $('#stars').css({
+        'animation-duration': duration + 's'
+    });
+
+    for (let d of decorations) {
+        if (d.min <= count && d.$obj.css('display') != 'block') {
+            d.$obj.fadeIn();
+        }
+        if (d.max <= count && d.$obj.css('display') == 'block') {
+            d.$obj.fadeOut();
+        }
+    }
+}
+
+function updateOthers(num) {
+    while ($('.other-button').length < num - 1) {
+        $('#others').append('<div class="other-button"></div>');
+    }
+    while ($('.other-button').length > num - 1) {
+        $('.other-button')[0].remove();
+    }
+    $('#viewer-counter').text(num - 1);
+}
+
+function animateButton() {
+    var $button = $('#he-button');
+    $button.finish();
+    $button.animate({
+        'font-size': '240%'
+    }, 100, function () {
+        $button.animate({
+            'font-size': '200%'
+        }, 100);
+    });
+}
+
+function pop() {
+    var $buttons = $('.other-button');
+    var random = $buttons[Math.floor(Math.random() * $buttons.length)];
+    $(random).css({ 'opacity': 1.0 });
+    $(random).animate({ 'opacity': 0.5 },500);
+}
+
+function isKiriban(num) {
+    var str = num.toString(10);
+    if (/^[1-9]0{2,}$/.test(str)) {
+        return true;
+    }
+    if (/^([1-9])\\1{2,}$/.test(str)) {
+        return true;
+    }
+    if (num > 100 && str == '123456789'.substr(0, str.length)) {
+        return true;
+    }
+    return false;
+}
+
+updateBackground(parseInt($('#he-counter').text(), 10));
---
+++ server.js
@@ -0,0 +1,102 @@
+﻿'use strict';
+
+const serve = require('koa-static');
+const _ = require('koa-route');
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const IO = require('koa-socket-2');
+const Pug = require('koa-pug');
+const nanoid = require('nanoid');
+
+const app = new Koa();
+const io = new IO();
+const pug = new Pug({
+    app: app,
+    viewPath: './views',
+    basedir: './views'
+});
+
+var rooms = {};
+class Room {
+    constructor(title) {
+        this.title = title;
+        this.count = 0;
+        this.viewer = 0;
+    }
+}
+
+const routes = {
+    home: (ctx) => {
+        ctx.render('home', {'rooms':rooms});
+    },
+
+    room: (ctx, connection) => {
+        if (rooms[connection]) {
+            ctx.render('room', { 'room': rooms[connection] ,'connection': connection });
+        } else {
+            ctx.throw(404);
+        }
+    },
+
+    create: (ctx) => {
+        var title = ctx.request.body.title;
+        if (!title) {
+            ctx.throw(400);
+        } else if (title.length > 100) {
+            ctx.throw(400);
+        } else {
+            var connection = nanoid();
+            rooms[connection] = new Room(ctx.request.body.title);
+            ctx.redirect('/' + connection);
+        }
+    }
+}
+
+const chat = {
+    join: (ctx, connection) => {
+        if (rooms[connection]) {
+            rooms[connection].viewer++;
+            ctx.socket.join(connection);
+            io.to(connection).emit('join', rooms[connection].viewer);
+        }
+    },
+
+    increment: (ctx) => {
+        var connections = ctx.socket.rooms;
+        for (var connection in connections) {
+            if (rooms[connection]) {
+                rooms[connection].count++;
+                ctx.socket.broadcast.to(connection).emit('increment', rooms[connection].count);
+            }
+        }
+    },
+
+    disconnecting: (ctx, reason) => {
+        var connections = ctx.socket.rooms;
+        for (var connection in connections) {
+            if (rooms[connection]) {
+                rooms[connection].viewer--;
+                if (rooms[connection].viewer == 0) {
+                    delete rooms[connection];
+                } else {
+                    io.to(connection).emit('leave', rooms[connection].viewer);
+                }
+            }
+        }
+    }
+}
+
+app.use(koaBody());
+app.use(serve(__dirname + '/public'));
+
+app.use(_.get('/', routes.home));
+app.use(_.post('/', routes.create));
+app.use(_.get('/:connection', routes.room));
+
+io.attach(app);
+io.on('join', chat.join);
+io.on('increment', chat.increment);
+io.on('disconnecting', chat.disconnecting);
+
+app.listen(process.env.PORT || 1337);
+console.log('listening on port 1337');
