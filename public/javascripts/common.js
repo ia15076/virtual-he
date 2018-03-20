f648e7463406153737d442156cada748541ddb1b
ia15076 <ia15076@s.inf.shizuoka.ac.jp>
2018-03-20 18:38:52 +0900

    説明を追加、プリロード画面を追加

---
+++ public/javascripts/common.js
@@ -0,0 +1,8 @@
+$("#loading").text("complete!");
+$("#preload").fadeOut();
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-116043944-1');
